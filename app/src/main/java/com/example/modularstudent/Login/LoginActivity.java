f6d37c97623525c4a921dd4ce4fb42c87e124b94
leomuko <kezzieleo@gmail.com>
2020-08-27 09:58:35 +0300

    Implement google login

---
+++ app/src/main/java/com/example/modularstudent/Login/LoginActivity.java
@@ -0,0 +1,124 @@
+package com.example.modularstudent.Login;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.modularstudent.MainActivity;
+import com.example.modularstudent.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SignInButton mSignInButton;
+    private FirebaseAuth mMAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private Integer REQUEST_CODE_SIGN_IN = 1;
+    private static final String TAG = "LoginActivity";
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignInButton = findViewById(R.id.google_sign_in_button);
+        mMAuth = FirebaseAuth.getInstance();
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+        initGoogleSignIn();
+    }
+
+    private void initGoogleSignIn() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            progressDialog = ProgressDialog.show(this, "","Please Wait...", true);
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount googleSignInAccount = task.getResult(ApiException.class);
+                if (googleSignInAccount != null) {
+                    getGoogleAuthCredential(googleSignInAccount);
+                }
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getGoogleAuthCredential(GoogleSignInAccount googleSignInAccount) {
+        String googleTokenId = googleSignInAccount.getIdToken();
+        AuthCredential googleAuthCredential = GoogleAuthProvider.getCredential(googleTokenId, null);
+        signInWithGoogleAuthCredential(googleAuthCredential);
+    }
+
+    private void signInWithGoogleAuthCredential(AuthCredential googleAuthCredential) {
+        mMAuth.signInWithCredential(googleAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                FirebaseUser firebaseUser = mMAuth.getCurrentUser();
+                if(firebaseUser != null){
+                    Log.d(TAG, "onComplete: Success");
+                    goToMainActivity();
+                }else {
+                    Log.d(TAG, "onComplete: "+ task.getException().getMessage());
+                    Toast.makeText(LoginActivity.this, "An error occured", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        progressDialog.dismiss();
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
+
+
--- app/src/main/java/com/example/modularstudent/Splash/SplashActivity.java
+++ app/src/main/java/com/example/modularstudent/Splash/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.example.modularstudent;
+package com.example.modularstudent.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.modularstudent.Login.LoginActivity;
+import com.example.modularstudent.MainActivity;
+import com.example.modularstudent.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
@@ -19,10 +22,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(mAuth != null){
+                if(mAuth.getCurrentUser() != null){
                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
-
                 }else{
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
 
                 }
                 finish();
