20f70494578988be9ef400aec80af2245ad35fb1
leomuko <kezzieleo@gmail.com>
2020-08-27 07:41:15 +0300

    Create splash screen -- Add firebase dependencies

---
+++ app/src/main/java/com/example/modularstudent/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.modularstudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(mAuth != null){
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+                }else{
+
+                }
+                finish();
+            }
+        }, 4000);
+    }
+}
